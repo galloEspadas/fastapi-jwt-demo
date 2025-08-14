443f33492c710e038679a5306cac392973f118ee
galloEspadas <antonio.hespadas@gmail.com>
2025-08-13 19:35:55 -0600

    fix type

--- app/utils.py
+++ app/utils.py
@@ -28,7 +28,7 @@ oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/auth/login")
 def get_current_user(token: str = Depends(oauth2_scheme)):
     try:
         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-        username: str = payload.get("sub")
+        username = payload.get("sub")
         if username is None:
             raise HTTPException(
                 status_code=status.HTTP_401_UNAUTHORIZED,
